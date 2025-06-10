aa692243cc7a5a3e429a2777f04eb4304363b41c
Eyal Ben Ivri <eyalbenivri@google.com>
2025-06-10 14:34:11 +0200

    small format for the cleanup script

--- src/open_data_lakehouse_demo/clean-notebooks.py
+++ src/open_data_lakehouse_demo/clean-notebooks.py
@@ -2,26 +2,21 @@ import json
 import os
 import pathlib
 
-import ruff
-
 
 def clean_notebooks(notebook_path: str):
     with open(notebook_path, "r") as fp:
         notebook_json = json.load(fp)
     cells = notebook_json["cells"]
     for cell in cells:
+        cell["execution_count"] = None
         if cell["cell_type"] == "code":
-            cell["execution_count"] = None
             cell["outputs"] = []
     notebook_json["cells"] = cells
     with open(notebook_path, "w") as fp:
         json.dump(notebook_json, fp)
 
 
-
-
-
-if __name__ == "__main__":
+def run():
     current_dir = pathlib.Path(os.getcwd())
     notebooks_dir = os.path.join(current_dir.parent.parent, "notebooks")
     for root, dirs, files in os.walk(notebooks_dir):
@@ -30,3 +25,7 @@ if __name__ == "__main__":
                 notebook_path = os.path.join(root, file)
                 print(f"Cleaning outputs from {notebook_path}")
                 clean_notebooks(notebook_path)
+
+
+if __name__ == "__main__":
+    run()
